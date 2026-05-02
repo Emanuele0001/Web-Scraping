77c782be47693385087569f48d96b4f5a6ac89aa
Emanuele0001 <139679365+Emanuele0001@users.noreply.github.com>
2026-05-02 17:44:27 +0200

    Implement CSV export for book data
    Added functionality to save book titles and prices to a CSV file.

--- BookScrape.py
+++ BookScrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 url = "https://books.toscrape.com/catalogue/category/books/science_22/index.html"
 response = requests.get(url)
@@ -11,8 +12,16 @@ soup = BeautifulSoup(response.text, 'html.parser')
 books = soup.find_all('article', class_='product_pod')
 
 for book in books:
+    
     tittle = book.h3.a['title']
     price = book.find('p', class_='price_color').text
     print(f'Title: {tittle}, Price: {price}')
     
-    
+    with open('books.csv', "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(['Title', 'Price'])
+        
+        for book in books:
+            tittle = book.h3.a['title']
+            price = book.find('p', class_='price_color').text
+            writer.writerow([tittle, price])
